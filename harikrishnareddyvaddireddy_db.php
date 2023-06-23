d8f1ce5644ff85079795c3783fad3b8353f9b1fb
Vaddireddy005 <vharikrishnareddy007@gmail.com>
2023-06-23 13:32:45 +0530

    first

---
+++ harikrishnareddyvaddireddy_db.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost="sql303.epizy.com";
+$dbuser="epiz_32657332";
+$dbpass="INSjdE4eiiwI";
+$conn=mysql_connect($dbhost,dbuser,dbpass);
+mysql_select_db("epiz_32657332_harikrishnareddyvaddireddy_db",$conn);
